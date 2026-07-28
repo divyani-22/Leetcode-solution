693f11e493dbe4a989d06a9482b03329baa031d4
Divyani <divyanipapalkar22@gmail.com>
2026-07-29 00:37:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (84.96%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+    int sum = 0;
+    int n = nums.size();
+    for(int i = 0; i < n; i++){
+        bool isUnique = true;
+        for(int j = 0; j < n; j++){
+            if(i != j && nums[i] == nums[j]){
+                isUnique = false;
+                break;
+                }
+            }            
+            if(isUnique){
+                sum += nums[i];
+            }
+        }        
+        return sum;        
+    }
+};
