8bc31335be48e84614ebd5326bed97a6bcb67472
Divyani <divyanipapalkar22@gmail.com>
2026-08-19 19:10:44 +0530

    Time: 39 ms (96.46%) | Memory: 150.9 MB (93.15%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,46 @@
+class MinStack {
+public:
+    stack<long long> st; 
+    long long mini;
+
+    MinStack() {
+        mini = LLONG_MAX;
+    }
+    
+    void push(int value) {
+        if (st.empty()) {
+            mini = value;
+            st.push(value);
+        } else {
+            if (value >= mini) {
+                st.push(value);
+            } else {
+                st.push(2LL * value - mini);
+                mini = value;
+            }
+        }
+    }
+    
+    void pop() {
+        if (st.empty()) return;
+        
+        long long x = st.top(); 
+        st.pop();
+        
+        if (x < mini) {
+            mini = 2LL * mini - x;
+        }
+    }
+    
+    int top() {
+        if (st.empty()) return -1;
+        
+        long long x = st.top(); 
+        if (mini > x) return mini;
+        return x;
+    }
+    
+    int getMin() {
+        return mini;
+    }
+};
